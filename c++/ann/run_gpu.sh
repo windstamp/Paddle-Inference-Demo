b05f9edc6d1d29e91ffc3133dcd756dc760c5c14
root <root@yq01-qianmo-com-255-129-13.yq01.baidu.com>
2021-06-17 02:46:59 +0000

    add run_cpu.sh

---
+++ c++/ann/run_gpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set +x
+set -e
+
+work_path=$(dirname $(readlink -f $0))
+
+# 1. compile
+bash ${work_path}/compile.sh
+
+# 2. download model
+if [ ! -d infer_model ]; then
+    echo -e "\033[31m Cannot find infer model, please download and rename it into infer_model dir. \033[0m"
+    exit 0
+fi
+
+# 3. run
+./build/ann_predict --model_file infer_model/model.pdmodel --params_file infer_model/model.pdiparams --use_gpu true
